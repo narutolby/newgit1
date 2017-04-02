96a3e7326770704d7ba3a21bdd6a56a004ddfe0e
铭壬 <boyang.lby@alibaba-inc.com>
2017-04-02 11:07:01 +0800

    WordLadderII solution

---
+++ src/WordLadderII.java
@@ -0,0 +1,136 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+
+/**
+ * @author boyang.lby
+ * @date 3/23/17
+ * @description dfs + backtracking
+ */
+public class WordLadderII {
+
+    List<List<String>> retList = new ArrayList<>();
+    Set<String> markedSet = new HashSet<>();
+    int shortestPath = Integer.MAX_VALUE;
+
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        Stack<String> stack = new Stack<>();
+        Set<String> wordSet = new HashSet<>(wordList);
+        shortestPath = ladderLength(beginWord,endWord,wordList);
+        if(shortestPath == 0){
+            return retList;
+        }
+        stack.push(beginWord);
+        dfs(beginWord, endWord, 1, stack, wordSet);
+        return retList;
+    }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+        Map<String, Integer> levelMap = new HashMap<String, Integer>();
+        Set<String> wordSet = new HashSet<>(wordList);
+        Set<String> marked = new HashSet<>();
+        Queue<String> queue = new LinkedList<String>();
+        queue.offer(beginWord);
+        levelMap.put(beginWord, 1);
+        while (!queue.isEmpty()) {
+            String word = queue.poll();
+            char[] sourceWordArray = word.toCharArray();
+            for (int i = 0; i < sourceWordArray.length; i++) {
+                char tmpChar = sourceWordArray[i];
+                for (char c = 'a'; c <= 'z'; c++) {
+                    if (c == tmpChar) {
+                        continue;
+                    }
+                    sourceWordArray[i] = c;
+                    String tmpWord = new String(sourceWordArray);
+                    if(wordSet.contains(tmpWord) && !marked.contains(tmpWord)){
+                        levelMap.put(tmpWord, levelMap.get(word) + 1);
+                        if (tmpWord.equals(endWord)) {
+                            return levelMap.get(tmpWord);
+                        }
+                        marked.add(tmpWord);
+                        queue.offer(tmpWord);
+                    }
+                }
+                sourceWordArray[i] = tmpChar;
+            }
+        }
+        return 0;
+    }
+
+    public void dfs(String sourceWord, String targetWord, int path, Stack<String> stack, Set<String> wordSet) {
+        if (path > shortestPath) {
+            return;
+        }
+        char[] sourceWordArray = sourceWord.toCharArray();
+        for (int i = 0; i < sourceWordArray.length; i++) {
+            char tmpChar = sourceWordArray[i];
+            for (char c = 'a'; c <= 'z'; c++) {
+                if (c == tmpChar) {
+                    continue;
+                }
+                sourceWordArray[i] = c;
+                String word = new String(sourceWordArray);
+                if (wordSet.contains(word) && !markedSet.contains(word)) {
+                    markedSet.add(word);
+                    stack.push(word);
+                    try {
+                        if (word.equals(targetWord)) {
+                            if (path < shortestPath) {
+                                shortestPath = path;
+                                retList.clear();
+                            }
+                            List<String> elementList = new ArrayList<>();
+                            elementList.addAll(stack);
+                            retList.add(elementList);
+                            return;
+                        } else {
+                            dfs(word, targetWord, path + 1, stack, wordSet);
+                        }
+                    } finally {
+                        stack.pop();
+                        markedSet.remove(word);
+                    }
+                }
+            }
+            sourceWordArray[i] = tmpChar;
+
+        }
+    }
+
+    public boolean isOneLetterDiff(String word, String word1) {
+        int diffCount = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == word1.charAt(i)) {
+                continue;
+            } else {
+                diffCount++;
+                if (diffCount > 1) {
+                    return false;
+                }
+            }
+        }
+        return diffCount == 0 ? false : true;
+    }
+
+    public static void main(String[]args){
+        String beginword= "hit";
+        String endword= "cog";
+        List<String> wordList = new ArrayList<String>();
+        wordList.add("hot");
+        wordList.add("dot");
+        wordList.add("dog");
+        wordList.add("lot");
+        wordList.add("log");
+        wordList.add("cog");
+        new WordLadderII().findLadders(beginword,endword,wordList);
+    }
+}
---
+++ src/WordLadderIIIII.java
@@ -0,0 +1,89 @@
+/**
+ * @author boyang.lby
+ * @date 4/2/17
+ * @description
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+
+/**
+ * the five times to try
+ */
+public class WordLadderIIIII {
+
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        List<List<String>> retList = new ArrayList<>();
+        if(wordList.isEmpty()){
+            return retList;
+        }
+        Map<String,List<String>> parantMap = new HashMap<>();
+        Map<String,Integer> shortestMap = new HashMap<>();
+        Queue<String> queue = new LinkedList<>();
+        Set<String> wordSet = new HashSet<>();
+        wordSet.addAll(wordList);
+        wordSet.remove(beginWord);
+        shortestMap.put(beginWord, 0);
+        queue.add(beginWord);
+        while(!queue.isEmpty()){
+            String word = queue.poll();
+            char[] chars = word.toCharArray();
+            for(int i=0;i<chars.length;i++){
+                char tmpChar = chars[i];
+                for(char c = 'a';c<='z';c++){
+                    if(c == tmpChar){
+                        continue;
+                    }else{
+                        chars[i] = c;
+                    }
+                    String adjcentWord = new String(chars);
+                    if(wordSet.contains(adjcentWord)){
+                        if(!shortestMap.containsKey(adjcentWord)){
+                            shortestMap.put(adjcentWord,shortestMap.get(word)+1);
+                            List<String> parentList = new ArrayList<>();
+                            parentList.add(word);
+                            parantMap.put(adjcentWord,parentList);
+                            queue.offer(adjcentWord);
+                        }else{
+                            if(shortestMap.get(word) + 1 == shortestMap.get(adjcentWord)){
+                                parantMap.get(adjcentWord).add(word);
+                            }
+                        }
+                    }
+                }
+                chars[i] = tmpChar;
+            }
+        }
+        if(!shortestMap.containsKey(endWord)){
+            return retList;
+        }
+        Stack<String> stack = new Stack<>();
+        stack.push(endWord);
+        recursivePrint(parantMap, endWord, retList, stack);
+        return retList;
+    }
+
+    public void recursivePrint(Map<String,List<String>> parentMap,String word,List<List<String>> retList,Stack<String> stack){
+        if(!parentMap.containsKey(word)){
+            LinkedList<String> linkedList = new LinkedList<>();
+            for(int i=0;i<stack.size();i++){
+               linkedList.addFirst(stack.get(i));
+            }
+            retList.add(linkedList);
+            return;
+        }
+        List<String> parentList = parentMap.get(word);
+        for(String parentWord : parentList){
+            stack.push(parentWord);
+            recursivePrint(parentMap, parentWord, retList, stack);
+            stack.pop();
+        }
+    }
+}
