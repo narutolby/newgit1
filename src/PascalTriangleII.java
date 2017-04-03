9fcd3cde11b2dad84db26d6446b6b9b67ba5b73d
铭壬 <boyang.lby@alibaba-inc.com>
2017-04-03 13:00:47 +0800

    Pascal's Triangle II

--- src/PascalTriangleII.java
+++ src/PascalTriangleII.java
@@ -30,7 +30,21 @@ public class PascalTriangleII {
         }
         return ret;
     }
+
+    public List<Integer> getRow1(int rowIndex) {
+        List<Integer> list = new ArrayList<Integer>();
+        if (rowIndex < 0){
+            return list;
+        }
+        for (int i = 0; i < rowIndex + 1; i++) {
+            list.add(0, 1);
+            for (int j = 1; j < list.size() - 1; j++) {
+                list.set(j, list.get(j) + list.get(j + 1));
+            }
+        }
+        return list;
+    }
     public static void main(String[]args){
-        new PascalTriangleII().getRow(0);
+        new PascalTriangleII().getRow1(3);
     }
 }
