3cfe35f31f4c31193de6372ea155744c49ca5275
铭壬 <boyang.lby@alibaba-inc.com>
2017-10-02 15:59:17 +0800

    Solve Equation

--- src/SolvetheEquation.java
+++ src/SolvetheEquation.java
@@ -2,6 +2,15 @@
  * @author boyang.lby
  * @date 10/2/17
  * @description
+ * 思路:
+ * 整体思路和解数学公式的思路是一样的,数字进行相加减(dig标识加减结果),x的系数进行相加减(co标识相加减结果),用一个s的变量来标识当前遍历是等号
+ * 左边还是右边,s=1代表等号左边,s=-1代表等号右边,sign标识符号,当遍历遇到'x',把计算好的num赋值给co,因为此时的num一定是x的系数,存在特殊
+ * case是'0x=0'和x没有系数的情况(此时x的系数为1),需要特殊判断'x'前一个字符是否为'0',不为0,则需要判断num是否为0
+ * (e.charAt(i-1) == '0'?0:num == 0?1:num)来决定x的系数,最后遍历完的结果是 co * x (+/-) dig = 0,如果co和dig都为0,则代表x可以任意取值,
+ * 返回"Infinite solutions";如果co为0,dig不为0,则 "No solution";如果上述都不是,则 x = (0 - dig) / co.
+ *
+ *
+ *
  */
 public class SolvetheEquation {
     public String solveEquation(String e) {
