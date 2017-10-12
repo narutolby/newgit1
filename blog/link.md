f3b2871023c705f1a3add5d168dd14541ad05bcb
铭壬 <boyang.lby@alibaba-inc.com>
2017-10-13 00:16:42 +0800

    some book

---
+++ blog/link.md
@@ -0,0 +1,3 @@
+[三个月刷题拿到 Google offer](https://www.diycode.cc/topics/220)
+[system design](https://www.hiredintech.com/system-design/the-system-design-process/)
+[system design2-highscalability](http://highscalability.com/)
--- src/google/onsite20171011/AddStationToMinMaxLength.java
+++ src/google/onsite20171011/AddStationToMinMaxLength.java
@@ -1,4 +1,44 @@
 package google.onsite20171011;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class AddStationToMinMaxLength {
+
+    class Interval{
+        private int width;
+        private int count;
+        public Interval(int width,int count){
+            this.width = width;
+            this.count = count;
+        }
+    }
+
+    public int addGasStation(int k,int[]stations){
+        if(k == 0 || stations == null || stations.length == 0 ){
+            return 0;
+        }
+        PriorityQueue<Interval> pq = new PriorityQueue<Interval>(new Comparator<Interval>(){
+            @Override
+            public int compare(Interval i1,Interval i2){
+                return i2.width / i2.count - i1.width / i1.count;
+            }
+        });
+
+        for(int i=0;i<stations.length;i++){
+            pq.add(new Interval(stations[i],1));
+        }
+        while(k > 0){
+            Interval max = pq.poll();
+            max.count ++;
+            pq.add(max);
+            k--;
+        }
+        return pq.poll().width;
+
+    }
+    public static void main(String[]args){
+        System.out.println(new AddStationToMinMaxLength().addGasStation(3,new int[]{2,3,4}));
+    }
+
 }
