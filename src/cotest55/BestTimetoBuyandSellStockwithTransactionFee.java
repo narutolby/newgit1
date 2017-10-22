8e711627f031b607b0600e8b93fa6212ffa6bbb8
铭壬 <boyang.lby@alibaba-inc.com>
2017-10-23 00:03:17 +0800

    contest55

---
+++ src/cotest55/BestTimetoBuyandSellStockwithTransactionFee.java
@@ -0,0 +1,26 @@
+package cotest55;
+
+/**
+ * @author boyang.lby
+ * @date 10/22/17
+ * @description
+ */
+public class BestTimetoBuyandSellStockwithTransactionFee {
+    public int maxProfit(int[] prices, int fee) {
+        if(prices == null || prices.length == 0){
+            return 0;
+        }
+        int len = prices.length;
+        int[]dp = new int[len];
+        int max = 0-prices[0];
+        for (int i = 1; i <len; i++) {
+            dp[i] = Math.max(prices[i] + max - fee,dp[i-1]);
+            max = Math.max(max,0 - prices[i] + dp[i-1]);
+        }
+        return dp[prices.length - 1];
+    }
+
+    public static void main(String[]args){
+        System.out.println(new BestTimetoBuyandSellStockwithTransactionFee().maxProfit(new int[] {1,4,6,2,8,3,10,14} ,3));
+    }
+}
---
+++ src/cotest55/RangeModule.java
@@ -0,0 +1,9 @@
+package cotest55;
+
+/**
+ * @author boyang.lby
+ * @date 10/22/17
+ * @description
+ */
+public class RangeModule {
+}
